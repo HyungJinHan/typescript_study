5c1e18e3f65cbe3b2b0427ec5b5c45ef5012d5d3
HyungJinHan <han1210_36@naver.com>
2023-02-15 23:27:08 +0900

    interface의 사용법 정리

--- src/interfaceExample2.ts
+++ src/interfaceExample2.ts
@@ -1,19 +1,59 @@
+// --- class형 ---
 abstract class UserClassEx {
   constructor(
-    protected firstname: string,
-    protected lastname: string
+    protected firstName: string,
+    protected lastName: string
   ) { }
 
   abstract sayHi(name: string): string
   abstract fullName(): string
 }
 
-class PlayerClassEX extends UserClassEx {
+class PlayerClassEx extends UserClassEx {
   fullName() {
-    return `${this.firstname} ${this.lastname}`
+    return `${this.firstName} ${this.lastName}`
   }
 
   sayHi(name: string) {
     return `Hello ${name}. I'm ${this.fullName()}`
   }
 }
+
+// --- interface형 ---
+interface UserInterfaceEx {
+  firstName: string,
+  lastName: string,
+  sayHi(name: string): string
+  fullName(): string
+}
+
+interface HumanInterfaceEx {
+  age: number
+}
+
+class PlayerInterfaceEx implements UserInterfaceEx, HumanInterfaceEx {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public age: number
+  ) { }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+
+  sayHi(name: string) {
+    return `Hello ${name}. I'm ${this.fullName()}`
+  }
+}
+
+function makeUser(user: UserInterfaceEx): UserInterfaceEx {
+  return {
+    firstName: 'Hyun Jin',
+    lastName: 'Han',
+    fullName: () => 'xx',
+    sayHi: (name) => 'string'
+  }
+}
+
+console.log(makeUser())
