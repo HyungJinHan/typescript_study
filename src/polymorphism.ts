2f9aa6e97735b06f798352d8da7c0970d3226d01
HyungJinHan <han1210_36@naver.com>
2023-02-16 15:47:33 +0900

    다형성 이해를 위한 localstorage api 디자인 예제

---
+++ src/polymorphism.ts
@@ -0,0 +1,23 @@
+interface MyStorage<T> {
+  [key: string]: T
+}
+
+class LocalStorage<T> {
+  private storage: MyStorage<T> = {}
+
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+
+  remove(key: string) {
+    delete this.storage[key]
+  }
+
+  get(key: string): T {
+    return this.storage[key]
+  }
+
+  clear() {
+    this.storage = {}
+  }
+}
